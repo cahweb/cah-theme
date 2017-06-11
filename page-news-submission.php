a50000cb1c9c7fde0ec21ab368d68c2d4001b2b8
Ale Vecchi <ale@odessy.local>
2017-06-11 12:27:57 -0400

    testing phpmailer

---
+++ page-news-submission.php
@@ -0,0 +1,46 @@
+<?php
+/**
+* Template Name: News Submission Page Template
+*
+* Description: A page template that provides a key component of WordPress as a CMS
+* by meeting the need for a carefully crafted introductory page. The front page template
+* in Twenty Twelve consists of a page content area for adding text, images, video --
+* anything you'd like -- followed by front-page-only widgets in one or two columns.
+*
+*/
+?>
+
+<?php get_header(); 
+	
+	$mail = new PHPMailer();
+	
+?>
+
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<header class="entry-header">
+                        <h1 class="entry-title"><? echo get_the_title(); ?></h1>
+                        <hr class="yellow-line">
+        </header><br>
+
+        <form name="news-form" method="post" action="#">
+        	<label for="headline">Headline:</label>
+        	<input type="text" name="headline">
+        	<input type="submit" value="Submit" name="submit" id="sub">
+        </form>
+
+	</main><!-- #main -->
+        
+</div>
+
+<?php
+
+get_footer();
+?>
+
+
+
+
+
